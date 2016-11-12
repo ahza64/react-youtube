d01c7441ebfc9f20b7a95073427c669682b5a1de
ahza64 <ahza64@yahoo.com>
2016-11-11 21:13:02 -0800

    connecting to youtube api

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Key from '../key';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = Key.key;
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
+    console.log(data);
+});
 
 // create new Component
 const App = () => {
