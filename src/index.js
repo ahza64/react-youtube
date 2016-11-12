3f4af38bba3788d2b82d1279624c0d3eacfd517d
ahza64 <ahza64@yahoo.com>
2016-11-11 21:22:37 -0800

    seting up state

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Key from '../key';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = Key.key;
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
-    console.log(data);
-});
-
 // create new Component
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.setState({ videos });
+        //this.setState({ videos: videos });
+    });
+  }
+  render () {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // generate html
