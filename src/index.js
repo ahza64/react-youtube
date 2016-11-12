8a25429e35e7eec5751c3a45dfb8163ce780afd4
ahza64 <ahza64@yahoo.com>
2016-11-11 22:22:19 -0800

    display data

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ class App extends Component {
 
     this.state = {videos: []};
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    YTSearch({key: API_KEY, term: 'evil dead'}, (videos) => {
         this.setState({ videos });
         //this.setState({ videos: videos });
         console.log(videos);
