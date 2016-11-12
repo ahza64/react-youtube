1cc5441af3a76d9adb5b660a313027834301169a
ahza64 <ahza64@yahoo.com>
2016-11-11 21:30:46 -0800

    passing props down to child

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import Key from '../key';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = Key.key;
 
 // create new Component
@@ -22,6 +23,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
